701a19ac6ffd914cf98a34b285540f05625eaeab
VivekSharma811 <vs811861@gmail.com>
2021-05-23 15:05:20 +0530

    Final Commit

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Vivek Sharma on 23/05/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageVIew: UIImageView!
+}
